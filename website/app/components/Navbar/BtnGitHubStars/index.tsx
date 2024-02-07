b45c29b4b0ac1c088f778f914af95f44ab9ba17c
Nelson Hernandez <nelsonher019@gmail.com>
2024-02-06 21:57:28 -0600

    [FEAT] Button GitHub Stars

---
+++ website/app/components/Navbar/BtnGitHubStars/index.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { Link } from "@remix-run/react";
+
+function BtnGitHubStars() {
+  const [Stars, setStars] = useState(0);
+
+  useEffect(() => {
+    const fetchStars = async () => {
+      const res = await fetch(
+        "https://api.github.com/repos/nelsondev19/countries.dev"
+      );
+
+      if (res.status === 200) {
+        const { stargazers_count } = await res.json();
+
+        setStars(stargazers_count);
+      }
+    };
+
+    fetchStars();
+  }, []);
+  return (
+    <Link
+      to="https://github.com/nelsondev19/countries.dev"
+      target="_blank"
+      className="p-2 gap-2 text-sm max-w-52 flex justify-center items-center bg-transparent-effect duration-300 text-center font-semibold rounded-xl border border-gray-700 hover:border-gray-600"
+      rel="noreferrer"
+    >
+      <svg
+        stroke="currentColor"
+        fill="none"
+        strokeWidth={2}
+        viewBox="0 0 24 24"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="size-5"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+        <path d="M9 18c-4.51 2-5-2-7-2" />
+      </svg>
+      Star on GitHub
+      <div className="bg-[var(--bg-btn)] text-black px-2 py-1 rounded-xl">
+        {Stars.toLocaleString("en-US")}
+      </div>
+    </Link>
+  );
+}
+
+export default BtnGitHubStars;
--- website/app/components/Navbar/index.tsx
+++ website/app/components/Navbar/index.tsx
@@ -1,38 +1,65 @@
 import { Link } from "@remix-run/react";
+import BtnGitHubStars from "./BtnGitHubStars";
 
 function Navbar() {
   return (
-    <nav className="backdrop-blur-lg py-7 p-3 z-50 sticky top-0 m-auto max-w-screen-md">
-      <div>
+    <>
+      <nav className="hidden md:block backdrop-blur-lg py-7 px-3 z-50 sticky top-0 m-auto max-w-screen-lg">
+        <div>
+          <div className="flex justify-between gap-10">
+            <div className="grid place-content-center">
+              <Link
+                to="/"
+                className="font-semibold hover:underline hover:text-gray-100 duration-300"
+              >
+                🌎 Countries API
+              </Link>
+            </div>
+            <div className="grid place-content-center">
+              <ul className="flex gap-3 justify-center items-center">
+                {paths.map(({ name, path }) => {
+                  return (
+                    <li key={`path-${name.toLowerCase()}`}>
+                      <Link
+                        to={path}
+                        className="font-semibold hover:underline hover:text-gray-100 duration-300"
+                        id={`link-${name.toLowerCase()}`}
+                      >
+                        {name}
+                      </Link>
+                    </li>
+                  );
+                })}
+
+                <BtnGitHubStars />
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <nav className="block md:hidden backdrop-blur-lg p-3 z-50 sticky top-0 m-auto max-w-screen-lg">
         <div className="flex justify-between gap-10">
           <div className="grid place-content-center">
             <Link
               to="/"
               className="font-semibold hover:underline hover:text-gray-100 duration-300"
             >
-              Countries API
+              🌎 Countries API
             </Link>
           </div>
           <div className="grid place-content-center">
-            <ul className="flex gap-3">
-              {paths.map(({ name, path }) => {
-                return (
-                  <li key={`path-${name.toLowerCase()}`}>
-                    <Link
-                      to={path}
-                      className="font-semibold hover:underline hover:text-gray-100 duration-300"
-                      id={`link-${name.toLowerCase()}`}
-                    >
-                      {name}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-7 fill-current"
+              viewBox="0 0 256 256"
+            >
+              <path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16ZM216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"></path>
+            </svg>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
 
@@ -57,6 +84,6 @@ const paths = [
   {
     id: "4",
     name: "About",
-    path: "/contact",
+    path: "/about",
   },
 ];
