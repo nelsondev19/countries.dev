c185db791c984a6fd3b14fe5c9058f96b2055cc9
Nelson Hernandez <nelsonher019@gmail.com>
2024-02-06 21:57:46 -0600

    [FEAT] Open Source section

--- website/app/root.tsx
+++ website/app/root.tsx
@@ -26,6 +26,11 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20100%20100%22%3E%3Ctext%20y%3D%22.9em%22%20font-size%3D%2290%22%3E%F0%9F%8C%8E%EF%B8%8F%3C%2Ftext%3E%3C%2Fsvg%3E"
+          type="image/svg+xml"
+        ></link>
         <Meta />
         <Links />
       </head>
--- website/app/routes/_index.tsx
+++ website/app/routes/_index.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
-import { handleCopyClick } from "~/functions/handleCopyClick";
+import { handleCopyClick } from "../functions/handleCopyClick";
+import BtnGitHubStars from "../components/Navbar/BtnGitHubStars";
 
 export const meta: MetaFunction = () => {
   return [
@@ -22,8 +23,10 @@ export default function Index() {
   return (
     <main className="p-3">
       <div className="h-[80vh] grid place-content-center">
-        <div className="flex justify-center items-center gap-2 px-3 py-[9px] mb-10 mx-80 w-auto bg-transparent-effect duration-300 text-center font-semibold rounded-xl border border-gray-700 hover:border-gray-600">
-          <span id="endpoint" className="text-sm">{text}</span>
+        <div className="flex justify-center items-center gap-2 px-3 py-[9px] mb-10 mx-5 md:mx-80 md:w-auto bg-transparent-effect duration-300 text-center font-semibold rounded-xl border border-gray-700 hover:border-gray-600">
+          <span id="endpoint" className="text-sm">
+            {text}
+          </span>
 
           <figure>
             {CopiedClipboard ? (
@@ -48,13 +51,13 @@ export default function Index() {
         </div>
         <h1
           id="title"
-          className="text-center text-6xl font-semibold text-gray-200"
+          className="text-center text-3xl md:text-6xl font-semibold text-gray-200"
         >
           Countries API
         </h1>
         <p
           id="description"
-          className="text-center text-xl max-w-screen-md m-auto mt-4"
+          className="text-center text-lg max-w-screen-sm m-auto mt-4"
         >
           A powerful public GraphQL API designed for developers and data
           enthusiasts who want to access country information easily. You can use
@@ -64,11 +67,57 @@ export default function Index() {
           <Link
             to="https://playground.countries.dev"
             target="_blank"
-            className="px-5 py-[9px] bg-transparent-effect duration-300 text-center font-semibold rounded-xl border border-gray-700 hover:border-gray-600"
+            className="p-2 gap-2 text-sm flex justify-center items-center bg-transparent-effect duration-300 text-center font-semibold rounded-xl border border-gray-700 hover:border-gray-600"
             rel="noreferrer"
           >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+              />
+            </svg>
             Try API Now
           </Link>
+        </div>
+      </div>
+
+      <div className="h-[50vh">
+        <h2
+          id="title-open-source"
+          className="text-2xl md:text-5xl gap-2 font-semibold text-gray-200 flex justify-center items-center"
+        >
+          Open Source{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="38.67258"
+            height="46.06185"
+            className="fill-current"
+            viewBox="0 0 38.67258 46.06185"
+          >
+            <path
+              d="M21.80088,45.33602c3.58974-4.38591,6.94814-8.95969,10.00375-13.73404,2.86574-4.47768,5.81646-9.27804,6.63859-14.62174,.73327-4.76619-.24381-9.90652-3.62485-13.51203C31.86184,.3154,27.08696-.858,23.0146,.65737c-2.17098,.80784-4.12471,2.29627-5.14654,4.41999-1.47908,3.07408-1.04556,6.39616-.49073,9.63746l4.56937-1.9264c-1.62023-3.70349-3.75608-7.572-7.86667-8.88482-3.77491-1.20561-8.11046,.30736-10.76739,3.13326C.17625,10.37272-.49135,15.10445,.30993,19.46737c.84114,4.57994,3.53759,8.51025,6.35773,12.10143,3.59938,4.58347,7.36162,9.04609,11.04794,13.55984,.85468,1.04652,2.64838,.88715,3.53553,0,1.03483-1.03483,.8568-2.48641,0-3.53553-2.78553-3.41077-5.57106-6.82154-8.35658-10.23231-2.40199-2.94115-4.87894-5.87361-6.52722-9.32514-1.37629-2.88196-2.01165-6.53576-.73129-9.56713,.98421-2.33022,3.45364-4.15895,6.01491-3.97726,3.28471,.23301,4.83431,4.20557,5.97841,6.82074,.49198,1.12456,2.09897,1.47253,3.12188,1.02289,1.2099-.53184,1.66396-1.68474,1.4475-2.94929-.31358-1.83195-.74253-3.99665-.01331-5.78466,.45475-1.11501,1.42193-1.85923,2.43997-2.22431,2.58585-.92733,5.40517-.15643,7.16385,2.22539,5.06337,6.85745-.62648,15.75519-4.42618,21.67044-2.79186,4.34627-5.82598,8.53065-9.09771,12.52801-.85449,1.04401-1.0215,2.51403,0,3.53553,.88184,.88184,2.67611,1.05004,3.53553,0h0Z"
+              origin="undraw"
+            />
+          </svg>
+        </h2>
+        <p
+          id="description-open-source"
+          className="text-center text-lg max-w-screen-sm m-auto mt-4"
+        >
+          Our database is available on GitHub - feel free to read, review, or
+          contribute to it however you want!.
+        </p>
+
+        <div className="flex justify-center items-center mt-5">
+          <BtnGitHubStars />
         </div>
       </div>
     </main>
